6b903062198f97977b2b1a36b1375eb7a67233ba
Ratu Raihan Amany <ratu.amany@shopee.com>
2022-11-10 19:35:06 +0700

    Refactor: modal on transfer and topup

--- frontend/src/pages/Topup.tsx
+++ frontend/src/pages/Topup.tsx
@@ -14,13 +14,9 @@ import {
   AmountForm,
   SelectForm,
 } from "../styles/Styled";
-import { ModalButton } from "../styles/Styled";
-import { Link } from "react-router-dom";
+
 
 function Topup() {
-  const formatBalance = (balance: number) => {
-    return balance.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
-  }
 
   const { user } = useSelector((state: RootState) => state.userReducer);
   const { topup, topupError, topupLoading } = useSelector(
@@ -120,133 +116,16 @@ function Topup() {
               Top Up
             </BlueButton>
             {topupError === "" ? (
-              <div
-                className={`modal ${modal ? "d-flex" : "d-none"}`}
-                tabIndex={-1}
-              >
-                <div className="modal-dialog">
-                  <div className="modal-content">
-                    <div className="modal-body">
-                      <div className="container">
-                        <div className="row mt-4">
-                          <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            width="55"
-                            height="55"
-                            fill="currentcolor"
-                            className="bi bi-check-circle-fill text-success"
-                            viewBox="0 0 16 16"
-                          >
-                            <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z" />
-                          </svg>
-                        </div>
-                        <div className="row mt-3">
-                          <div className="d-flex justify-content-center">
-                            <h1 className="fw-bold text-success">
-                              Top Up Success
-                            </h1>
-                          </div>
-                        </div>
-                        <div className="row mt-5">
-                          <div className="d-flex justify-content-between align-items-center">
-                            <p>Amount</p>
-                            <p className="fw-bold fs-3">{formatBalance(inputAmount)}</p>
-                          </div>
-                        </div>
-                        <div className="row">
-                          <div className="d-flex justify-content-between">
-                            <p>From</p>
-                            <p>{"100" + inputSelect}</p>
-                          </div>
-                        </div>
-                        <div className="row">
-                          <div className="d-flex justify-content-between">
-                            <p>To</p>
-                            <p>{user.wallet_number}</p>
-                          </div>
-                        </div>
-                        <div className="row">
-                          <div className="d-flex justify-content-between">
-                            <p>Description</p>
-                            <p>{descriptionModal(inputSelect)}</p>
-                          </div>
-                        </div>
-                        <div className="row my-3">
-                          <Link to={"/"} className="text-decoration-none">
-                            <div className="d-flex justify-content-center gap-4">
-                              <ModalButton
-                                type="button"
-                                className="btn"
-                                data-bs-dismiss="modal"
-                              >
-                                Print
-                              </ModalButton>
-                              <ModalButton
-                                type="button"
-                                className="btn"
-                                data-bs-dismiss="modal"
-                              >
-                                Close
-                              </ModalButton>
-                            </div>
-                          </Link>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
+              <ModalSuccess
+                typeTrans="Top Up"
+                amount={inputAmount}
+                from={user.wallet_number}
+                to={user.wallet_number}
+                description={descriptionModal(inputSelect)}
+                show={modal}
+              />
             ) : (
-              <div
-                className={`modal ${modal ? "d-flex" : "d-none"}`}
-                tabIndex={-1}
-              >
-                <div className="modal-dialog">
-                  <div className="modal-content">
-                    <div className="modal-body">
-                      <div className="container">
-                        <div className="row mt-4">
-                          <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            width="55"
-                            height="55"
-                            fill="currentColor"
-                            className="bi bi-x-circle-fill text-danger"
-                            viewBox="0 0 16 16"
-                          >
-                            <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293 5.354 4.646z" />
-                          </svg>
-                        </div>
-                        <div className="row mt-3">
-                          <div className="d-flex justify-content-center">
-                            <h1 className="fw-bold text-danger">
-                              Top Up Failed
-                            </h1>
-                          </div>
-                        </div>
-                        <div className="row mt-5">
-                          <div className="d-flex justify-content-center">
-                            <p className="fw-bold fs-3">{topupError}</p>
-                          </div>
-                        </div>
-                        <div className="row my-3">
-                          <div className="d-flex justify-content-center gap-4">
-                          <Link to={"/"} className="text-decoration-none">
-                            <ModalButton
-                              type="button"
-                              className="btn"
-                              data-bs-dismiss="modal"
-                            >
-                              Close
-                            </ModalButton>
-                            </Link>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
+              <ModalFailed transType="Top Up" error={topupError} show={modal} />
             )}
           </div>
         </form>
