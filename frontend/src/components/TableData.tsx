3afe8c767683bb4058705225b286ba02e479c5ec
Ratu Raihan Amany <ratu.amany@shopee.com>
2022-11-10 12:54:41 +0700

    Add: filter feature

--- frontend/src/components/TableData.tsx
+++ frontend/src/components/TableData.tsx
@@ -1,18 +1,97 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import Table from 'react-bootstrap/Table';
 import { useDispatch, useSelector } from 'react-redux';
 import { getAllTransactions } from '../redux/actions/transactionsAction';
 import { TransactionsDispatch } from '../redux/actions/typesActions';
 import { RootState } from '../redux/reducers/indexReducers';
 import moment from 'moment'
+import {SelectForm, SearchBar, SelectShowType} from '../styles/Styled'
+import { ITransaction } from '../interface';
 
 function TableData() {
   const {transactions, transactionsLoading, transactionsError} = useSelector((state: RootState) => state.transactionsReducer);
+ 
+  const [displayData, setDisplayData] = useState(transactions)
+  const [searchValue, setSearchValue] = useState("");
+  const [selectShowType, setSelectShowType] = useState("last-10-transaction");
+  const [selectSortType, setSelectSortType] = useState("descending");
+  const [selectSortBy, setSelectSortBy] = useState("date");
+  console.log(displayData)
+
   const dispatchTrans: TransactionsDispatch = useDispatch()
   console.log(transactions)
-  useEffect(() => {
-    dispatchTrans(getAllTransactions())
-  }, [dispatchTrans]);
+
+  function FilterShow(data: ITransaction[]) {
+    let filteredData:ITransaction[]
+    switch(selectShowType){
+        case "last-10-transactions":
+            filteredData = data.slice(0,10);
+            return filteredData;
+        case "this-month":
+            filteredData = data.filter((val) => moment(val.date).month() === moment().month());
+            return filteredData;
+        case "last-month":
+            filteredData = data.filter((val) => moment(val.date).month() === moment().subtract(1,"month").month());
+            return filteredData;
+        case "this-year":
+            filteredData = data.filter((val) => moment(val.date).year() === moment().year());
+            return filteredData;
+        case "last-year": 
+            filteredData = data.filter((val) => moment(val.date).year() === moment().subtract(1,"year").year());
+            return filteredData;
+        default:
+            filteredData = data.slice(0,10);
+            return filteredData;
+    }
+        
+  }
+
+  function FilterSortDate(data: ITransaction[]) {
+    let sortedData:ITransaction[];
+    switch(selectSortType){
+        case "ascending":
+            sortedData = data.sort((a, b) => moment(a.date).isAfter(moment(b.date))?1:-1);
+            return sortedData;
+        case "descending":
+            sortedData = data.sort((a, b) => moment(a.date).isAfter(moment(b.date))?-1:1);
+            return sortedData;
+        default:
+            sortedData = data.sort((a, b) => moment(a.date).isAfter(moment(b.date))?-1:1);
+            return sortedData;
+    }
+  }
+
+  function FilterSortAmount(data: ITransaction[]) {
+    let sortedData:ITransaction[];
+    switch(selectSortType){
+        case "ascending":
+            sortedData = data.sort((a, b) => a.amount - b.amount);
+            return sortedData;
+        case "descending":
+            sortedData = data.sort((a, b) =>  b.amount - a.amount);
+            return sortedData;
+        default:
+            sortedData = data.sort((a, b) =>  b.amount - a.amount);
+            return sortedData;
+    }
+  }
+
+  function FilterData() {
+    let filteredData = FilterShow(transactions);
+    let sortedData; 
+    switch(selectSortBy){
+        case "date":
+            sortedData = FilterSortDate(filteredData);
+            break; 
+        case "amount":
+            sortedData = FilterSortAmount(filteredData);
+            break; 
+        default:
+            sortedData = FilterSortDate(filteredData);
+            break; 
+    }
+    setDisplayData(sortedData);
+  }
 
   function formatType(transType:string) {
     if(transType == "INCOME"){
@@ -30,30 +109,71 @@ function TableData() {
     }
   }
 
+  useEffect(() => {
+    dispatchTrans(getAllTransactions())
+    FilterData()
+  }, [dispatchTrans]);
+
+  useEffect(() => {
+    FilterData()
+  }, [selectShowType, selectSortBy, selectSortType]);
+
   return (
-    <div>
-    <Table responsive striped>
-      <thead>
-        <tr>
-          <th>Date {"&"} Time</th>
-          <th>Type</th>
-          <th>From/To</th>
-          <th>Description</th>
-          <th>Amount</th>
-        </tr>
-      </thead>
-      <tbody>
-        {transactions.map(val => (
-            <tr>
-                <td>{moment(val.date).format('h:mm - D MMMM YYYY')}</td>
-                <td>{formatType(val.status)}</td>
-                <td>{val.sender} / {val.recipient}</td>
-                <td>{val.description}</td>
-                <td>{formatAmount(val.status,val.amount)}</td>
-            </tr>
-        ))}
-      </tbody>
-    </Table>
+    <div className='container'>
+        <div className='row mt-4'>
+            <div className='col'>
+                <div className='d-flex justify-content-start gap-2 align-items-center'>
+                <label htmlFor="Show">Show</label>
+                <SelectShowType className="form-select" id="show_type" onChange={(e)=> {setSelectShowType(e.target.value)}}>
+                        <option value='last-10-transactions' selected>Last 10 transactions</option>
+                        <option value='this-month'>This month</option>
+                        <option value='last-month'>Last month</option>
+                        <option value='this-year'>This year</option>
+                        <option value='last-year'>Last year</option>
+                </SelectShowType>
+                </div>
+            </div>
+            <div className='col'>
+                <div className='d-flex justify-content-between gap-2'>
+                <label htmlFor="Show">Sort by</label>
+                <SelectForm className="form-select" id="sort_by" onChange={(e) => {setSelectSortBy(e.target.value)}}>
+                        <option value='date' selected>Date</option>
+                        <option value='amount'>Amount</option>
+                </SelectForm>
+                <SelectForm className="form-select" id="sort_type" onChange={(e) => {setSelectSortType(e.target.value)}}>
+                        <option value='ascending'>Ascending</option>
+                        <option value='descending' selected>Descending</option>
+                </SelectForm>
+                <SearchBar type='text' placeholder='search...' onChange={(e) => {setSearchValue(e.target.value)}}/>
+                </div>
+            </div>
+        </div>
+        <div className='row mt-4'>
+            <Table responsive striped>
+                <thead>
+                    <tr>
+                    <th>Date {"&"} Time</th>
+                    <th>Type</th>
+                    <th>From/To</th>
+                    <th>Description</th>
+                    <th>Amount</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {displayData
+                    .filter((desc) => desc.description.toLowerCase().includes(searchValue))
+                    .map(val => (
+                        <tr>
+                            <td>{moment(val.date).format('h:mm - D MMMM YYYY')}</td>
+                            <td>{formatType(val.status)}</td>
+                            <td>{val.sender} / {val.recipient}</td>
+                            <td>{val.description}</td>
+                            <td>{formatAmount(val.status,val.amount)}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </Table>
+        </div>
     </div>
   )
 }
--- frontend/src/styles/Styled.ts
+++ frontend/src/styles/Styled.ts
@@ -1,4 +1,5 @@
 import styled from 'styled-components'; 
+import searchIcon from '../assets/search.png';
 
 export const InputForm = styled.input({
     border: '1px solid #555',
@@ -36,4 +37,20 @@ export const ModalButton = styled.button({
     width: '80px',
     color: '#4ea5ea',
     fontWeight: 'bold'
+})
+
+export const SearchBar = styled.input({
+    border: '1px solid #555',
+    borderRadius: '8px', 
+    height: '45px', 
+    display: 'block',
+    padding: '8px 2px 8px 40px', 
+    // backgroundImage: `url(${searchIcon})`
+})
+
+export const SelectShowType = styled.select({
+    border: '1px solid #555',
+    borderRadius: '8px', 
+    height: '45px',
+    width: '250px'
 })
